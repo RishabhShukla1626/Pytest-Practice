51cc3d8ca82e0baf4b5938a7167095d1d6eb841c
Rishabh Shukla <55676665+RishabhShukla1626@users.noreply.github.com>
2021-04-21 08:08:07 +1300

    updateing all the test cases to pytest native.

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -6,68 +6,47 @@ from companies.models import Companies
 import json
 import pytest
 
+companies_url = reverse("companies-list")
+client = Client()
 
 @pytest.mark.django_db
-class BasicCompanyAPITestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.companies_url = reverse("companies-list")
+def test_when_there_is_one_company(client):
+    company_obj = Companies.objects.create(name="Amazon")
+    response = client.get(companies_url)
+    response_content = json.loads(response.content)[0]
+    assert response.status_code == 200
+    assert response_content.get("name") == company_obj.name
+    assert response_content.get("status") == company_obj.status
 
-    def tearDown(self):
-        pass
+@pytest.mark.django_db
+def test_create_company_that_already_exists(client):
+    company_obj = Companies.objects.create(name="Google")
+    response = client.post(companies_url, data={"name": "Google"})
+    assert response.status_code == 400
+    assert json.loads(response.content) == {"name": ["companies with this name already exists."]}
 
+@pytest.mark.django_db
+def test_create_company_with_name_only(client):
+    response = client.post(companies_url, data={"name": "SnapChat"})
+    assert response.status_code == 201
+    response_content = json.loads(response.content)
+    assert response_content.get("name") == "SnapChat" 
+    assert response_content.get("status") == "Hiring" 
 
-class TestGetCompanies(BasicCompanyAPITestCase):
-    def test_zero_companies(self):
-        response = self.client.get(self.companies_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), [])
+@pytest.mark.django_db
+def test_create_company_with_layoffs(client):
+    response = client.post(
+        companies_url, data={"name": "SnapChat", "status": "Layoffs"}
+    )
+    assert response.status_code == 201 
+    response_content = json.loads(response.content)
+    assert response_content.get("status") == "Layoffs"
 
-    def test_when_there_is_one_company(self):
-        company_obj = Companies.objects.create(name="Amazon")
-        response = self.client.get(self.companies_url)
-        response_content = json.loads(response.content)[0]
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_content.get("name"), company_obj.name)
-        self.assertEqual(response_content.get("status"), company_obj.status)
-
-
-class TestPostCompanies(BasicCompanyAPITestCase):
-    def test_company_without_data(self):
-        response = self.client.post(self.companies_url)
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(
-            json.loads(response.content), {"name": ["This field is required."]}
-        )
-
-    def test_create_company_that_already_exists(self):
-        company_obj = Companies.objects.create(name="Google")
-        response = self.client.post(self.companies_url, data={"name": "Google"})
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(
-            json.loads(response.content),
-            {"name": ["companies with this name already exists."]},
-        )
-
-    def test_create_company_with_name_only(self):
-        response = self.client.post(self.companies_url, data={"name": "SnapChat"})
-        self.assertEqual(response.status_code, 201)
-        response_content = json.loads(response.content)
-        self.assertEqual(response_content.get("name"), "SnapChat")
-        self.assertEqual(response_content.get("status"), "Hiring")
-
-    def test_create_company_with_layoffs(self):
-        response = self.client.post(
-            self.companies_url, data={"name": "SnapChat", "status": "Layoffs"}
-        )
-        self.assertEqual(response.status_code, 201)
-        response_content = json.loads(response.content)
-        self.assertEqual(response_content.get("status"), "Layoffs")
-
-    def test_create_company_with_wrong_status(self):
-        response = self.client.post(
-            self.companies_url, data={"name": "SnapChat", "status": "WrongStatus"}
-        )
-        self.assertEqual(response.status_code, 400)
-        self.assertIn("WrongStatus", str(response.content))
-        self.assertIn("is not a valid choice.", str(response.content))
+@pytest.mark.django_db
+def test_create_company_with_wrong_status(client):
+    response = client.post(
+        companies_url, data={"name": "SnapChat", "status": "WrongStatus"}
+    )
+    assert response.status_code == 400
+    assert "WrongStatus" in str(response.content)
+    assert "is not a valid choice." in str(response.content)
